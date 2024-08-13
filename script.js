23624c3d9db6c1d732ddc1c1e284dae7f00b525d
calculadoradeedad <mariajoseguauna@iemmmrip.edu.co>
2024-08-12 20:26:40 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcularEdad() {
+    let dob = document.getElementById('dob').value;
+    if (!dob) {
+        alert("Por favor, ingresa tu fecha de nacimiento.");
+        return;
+    }
+
+    let fechaNacimiento = new Date(dob);
+    let hoy = new Date();
+    
+    let años = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    let meses = hoy.getMonth() - fechaNacimiento.getMonth();
+    let días = hoy.getDate() - fechaNacimiento.getDate();
+    
+    if (días < 0) {
+        meses--;
+        días += new Date(hoy.getFullYear(), hoy.getMonth(), 0).getDate();
+    }
+    
+    if (meses < 0) {
+        años--;
+        meses += 12;
+    }
+
+    document.getElementById('resultado').innerHTML = 'Tienes ' + años + ' años, ' + meses + ' meses, y ' + días + ' días.';
+}
